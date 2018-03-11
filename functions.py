98fb14b3bcaf7a78e5eed03aee5eff988e467ab4
Shuhei Kayawari <suruttok@gmail.com>
2018-03-12 05:18:53 +0900

    Add save_img_component and save_img functions

--- functions.py
+++ functions.py
@@ -1,74 +1,101 @@
 import serial, shutil, os;
+from distutils.dir_util import copy_tree;
+from datetime import datetime;
+from tqdm import tqdm;
 
-COM_NUM = 3
+#ポート番号
+COM_NUM = 6
 
 #シリアルポート設定
-ser = serial.Serial('COM' + str(COM_NUM), baudrate = 9600, bytesize = 8, parity = 'N', stopbits = 1, rtscts = 1);
+ser = serial.Serial('COM' + str(COM_NUM),
+                    baudrate = 115200,
+                    bytesize = 8,
+                    parity = 'N',
+                    stopbits = 1,
+                    rtscts = 1);
 
+#フォルダセットアップ関数
+def folder_setup():
+    #バックアップフォルダが存在しない場合は新規作成
+    if not os.path.isdir("CanSat_camera_data_BACKUP"):
+        os.mkdir("CanSat_camera_data_BACKUP");
 
-#ディレクトリが存在する場合は削除
-if os.path.isdir("CanSat_camera_data"):
-    shutil.rmtree("CanSat_camera_data");
+    #ディレクトリが存在する場合はファイル名に時刻を書き加えてバックアップフォルダに移動
+    if os.path.isdir("CanSat_camera_data"):
+        new_name = "CanSat_camera_data_%s" % datetime.now().strftime("%Y_%m_%d_%H_%M_%S");
+        os.rename("CanSat_camera_data", new_name);
+        shutil.move(new_name, "CanSat_camera_data_BACKUP");
 
-
-#ディレクトリ作成
-os.mkdir("CanSat_camera_data");
-
+    #受信データ保存用フォルダ新規作成
+    os.mkdir("CanSat_camera_data");
 
 #ダウンリンク確認関数
 def connection_confirm():
-    print("Start: connection_confirm");
+    print("↓ Start:   Connection Confirm");
     downlink_confirm_text = ser.read(5).decode('utf-8');
+    print(downlink_confirm_text);
 
     if downlink_confirm_text == 'Hello':
         ser.write(b'1');
 
-        print("Success: connection_confirm");
+        print("○ Success: Connection Confirm");
 
         return True;
     else:
         ser.write(b'0');
 
-        print("Failure: connection_confirm");
+        print("× Failure: Connection Confirm");
 
         return False;
 
 
-line_count = 0;
 
-#画像受け取り・保存関数
-def save_img(fo_test):
-    global line_count;
-
-    receive_img_data = ser.readline().replace(b'\xad', b'').replace(b'\x84', b'').replace(b'\x0f', b'').replace(b'}', b'').replace(b'm', b'').strip().decode('utf-8', 'ignore'); #一行データ受け取り
-
-    print("Receive line_data( %d )" % line_count);
-    print(receive_img_data);
-
-    receive_checksum = ser.readline().replace(b'\xad', b'').replace(b'\x84', b'').replace(b'\x0f', b'').replace(b'}', b'').replace(b'm', b'').strip().decode('utf-8', 'ignore'); #チェックサム受取
-    print("Receive checksum");
-
+#画像一部保存関数
+def save_img_component(fo_test, receive_data, receive_checksum):
     checksum = 0;
-    #一行分のデータを足し合わせる
-    for i in range(len(receive_img_data)):
-        checksum += ord(receive_img_data[i]);
 
-    print(checksum);
-    print(receive_checksum);
+    #受信データを足し合わせる
+    for i in range(len(receive_data)):
+        checksum += ord(receive_data[i]);
+
 
     if checksum == int(receive_checksum): #チェックサムが一致した時
-        fo_test.write(receive_img_data.strip().encode('utf-8')); #受信データを保存ファイルに追記
+        fo_test.write(receive_data.strip().encode('utf-8')); #受信データを保存ファイルに追記
 
         ser.write(b'1');
 
-        line_count = line_count + 1;
-
-        print("Checksums are same");
-
         return True;
     else:
         ser.write(b'0');
 
-        print("Checksums are different");
-
         return False;
+
+
+#画像保存関数
+def save_img(mission_type, img_num):
+    #画像枚数分繰り返し
+    for i in range(img_num):
+        print("↓ Start:   Save Image( %s ) %d / %d" % (mission_type, i + 1 , img_num));
+
+        pbar = tqdm(total = 587574);
+
+        save_img_pass = 'CanSat_camera_data/%s_img%d.bmp' % (mission_type, i);
+        fo = open(save_img_pass, 'ab');
+
+        receive_header_data = ser.readline().strip().decode('utf-8', 'ignore');
+        receive_checksum = ser.read(1).strip().decode('utf-8', 'ignore');
+
+        while save_img_component(fo, receive_header_data, receive_checksum) == False:
+            continue;
+        pbar.update(54);
+
+        #一行データ × 360回繰り返し
+        for i in range(360):
+            receive_img_line_data = ser.readline().strip().decode('utf-8', 'ignore'); #一行データ受け取り
+            receive_checksum = ser.read(1).strip().decode('utf-8', 'ignore');
+
+            while save_img_component(fo, receive_img_line_data, receive_checksum) == False:
+                continue;
+            pbar.update(544 * 3);
+
+        pbar.close();
--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import functions;
 from functions import *;
 
+folder_setup();
+print("Folder Setup");
+
 while 1:
     stdout_message = ser.readline().strip().decode('utf-8', 'ignore');
     print(stdout_message);
 
-    if stdout_message == "Transfer Sequence Start":
+    if stdout_message == "START: Transfer Sequence":
         break;
 
+
 #ダウンリンク確認
-# while connection_confirm() == False:
-#     continue;
+while connection_confirm() == False:
+    continue;
+
+
+sub_img_num = ser.read(1).strip().decode('utf-8', 'ignore');
+main_img_num = ser.read(1).strip().decode('utf-8', 'ignore');
 
 #サブミッション画像受け取り・保存
-print("Start: save_img( sub )");
-save_img_pass = 'CanSat_camera_data/sub_img.bmp';
-fo = open("test.txt", 'ab');
-print("File opened");
-save_img(fo);
+save_img('sub', sub_img_num);
 
 #メインミッション画像受け取り・保存
-# for i in range(360):
-#     while save_img("main") == False:
-#         continue;
+save_img('main', main_img_num);
